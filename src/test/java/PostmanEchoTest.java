f6b96d79d8009dc027f4a149615525ecd3842229
fdarcy <fdarcy@mail.ru>
2024-01-13 17:18:25 +0300

    fail

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnSendData() {
+        String text = "good";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("bad") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(text));
+        ;
+    }
+}
